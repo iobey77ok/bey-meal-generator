27f9c9b6557f4e65ca944cb8fa7d3511096baeee
iobey77ok <nattadonx@gmail.com>
2026-06-03 19:18:56 +0700

    feat: add new listening port at backend

--- backend/server.js
+++ backend/server.js
@@ -116,6 +116,11 @@ app.get('/health', (req, response) => {
 });
 
 // Starts backend at port 3000 we defined above
-app.listen(PORT, () => {
-  console.log(`Meal Generator API is running at http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Meal Generator API is running at http://localhost:${PORT}`);
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
